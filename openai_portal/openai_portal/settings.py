eb9fa973969457fc04262042147f8ce0826378ca
cmadajski <cmad7317@tutanota.com>
2023-01-12 16:34:23 -0500

    Remove generic secret key, use key from env instead

--- openai_portal/openai_portal/settings.py
+++ openai_portal/openai_portal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3)%9h-*%o4mec-^xzxn#d7ol0%_(!rqf@uw3=6zyod3n*(ptg*'
+SECRET_KEY = os.getenv('django_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
