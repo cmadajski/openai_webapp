02dee8d622a5e0aabda4ceb518e3868c70637c3f
cmadajski <cmad7317@tutanota.com>
2023-01-12 22:10:34 -0500

    Alter endpoint names for nginx testing

--- openai_portal/openai_services/views.py
+++ openai_portal/openai_services/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
+    return redirect('/openai/')
+
+def services(request):
     return render(request, 'openai_services/index.html')
